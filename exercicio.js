71471dcdd066f2773df3aa28ec894c667b09c3c0
andersongulartew <andersongularte1993@gmail.com>
2024-04-15 20:07:17 -0300

    .

--- exercicio.js
+++ exercicio.js
@@ -1,35 +1,15 @@
+// EXERCICIOS
+
 // 1) Converta o número 123.456 para uma string e depois para um inteiro.
+
 // 2) O que acontece quando você tenta somar um booleano com um número? Experimente com diferentes valores.
+
 //  3) Utilize coerção para converter uma string contendo "false" em um valor booleano. Como você faria isso?
 
+// 4) Tente concatenar com coerção um number com uma string. Vamos ver o que acontece ?
 
-// **** 1)
-// const num = 123.456;
 
-// const num2 = num.toString(); //numero em string
-// console.log(typeof num2);
 
-// const num3 = parseInt(num2); // string em numero inteiro
-// console.log(num3);
-// console.log(typeof num3);
-
-//////////////////////////////////////////////////////////////
-
-// **** 2)
-// let resultado1 = true + 5; //true é convertido em 1
-// console.log(resultado1);
-
-// let resultado2 = false + 5;  //false é convertido em 0
-// console.log(resultado2);
-
-///////////////////////////////////////////////////////////////
-
-// **** 3)
-let text = "false";
-
-let text1 = Boolean(text);
-console.log(text1);
-console.log(typeof text1)
 
 
 
